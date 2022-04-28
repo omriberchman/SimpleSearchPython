b2a6cef72f85945cbcc0fe034ac4c5d74219377d
Omri <67146455+omriberchman@users.noreply.github.com>
2022-04-28 19:56:16 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,5 +1,8 @@
+import os
+
 text = input("Enter your text ")
 lowertext = text.lower()
 word = input('insert a word to count ')
 count = lowertext.count(word)
 print(word+" is written "+ str(count)+" times")
+os.system('pause')
